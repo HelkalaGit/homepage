939abba5573e876fc2273c9e2bdfa6cdf6535235
joalhelk <joalhelk@jyu.fi>
2022-04-08 16:50:13 +0300

    initial commit

---
+++ .next/server/pages/index.js
@@ -0,0 +1,56 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/index";
+exports.ids = ["pages/index"];
+exports.modules = {
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _chakra_ui_react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @chakra-ui/react */ \"@chakra-ui/react\");\n/* harmony import */ var _chakra_ui_react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_chakra_ui_react__WEBPACK_IMPORTED_MODULE_1__);\n\n\nconst Page = ()=>{\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(_chakra_ui_react__WEBPACK_IMPORTED_MODULE_1__.Container, {\n        children: [\n            /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(_chakra_ui_react__WEBPACK_IMPORTED_MODULE_1__.Box, {\n                borderRadius: \"lg\",\n                children: \"Hello, I'm a full-stack developer based in Finland!\"\n            }, void 0, false, {\n                fileName: \"/Users/joelhelkala/Documents/dev/joelhomepage/pages/index.js\",\n                lineNumber: 6,\n                columnNumber: 9\n            }, undefined),\n            /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"div\", {\n                children: \"Hello!\"\n            }, void 0, false, {\n                fileName: \"/Users/joelhelkala/Documents/dev/joelhomepage/pages/index.js\",\n                lineNumber: 9,\n                columnNumber: 9\n            }, undefined)\n        ]\n    }, void 0, true, {\n        fileName: \"/Users/joelhelkala/Documents/dev/joelhomepage/pages/index.js\",\n        lineNumber: 5,\n        columnNumber: 8\n    }, undefined);\n};\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Page);\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy9pbmRleC5qcy5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7OztBQUFBO0FBQWlEO0FBRWpELE1BQU1FLElBQUksR0FBRyxJQUFNO0lBQ2hCLHFCQUNJLDhEQUFDRix1REFBUzs7MEJBQ1QsOERBQUNDLGlEQUFHO2dCQUFDRSxZQUFZLEVBQUMsSUFBSTswQkFBQyxxREFFekI7Ozs7O3lCQUFNOzBCQUNKLDhEQUFDQyxLQUFHOzBCQUFDLFFBQU07Ozs7O3lCQUFNOzs7Ozs7aUJBQ1QsQ0FDYjtDQUNGO0FBRUQsaUVBQWVGLElBQUkiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9qb2VsaG9tZXBhZ2UvLi9wYWdlcy9pbmRleC5qcz9iZWU3Il0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7IENvbnRhaW5lciwgQm94IH0gZnJvbSAnQGNoYWtyYS11aS9yZWFjdCdcblxuY29uc3QgUGFnZSA9ICgpID0+IHtcbiAgIHJldHVybiAoXG4gICAgICAgPENvbnRhaW5lcj5cbiAgICAgICAgPEJveCBib3JkZXJSYWRpdXM9XCJsZ1wiPlxuICAgICAgICBIZWxsbywgSSZhcG9zO20gYSBmdWxsLXN0YWNrIGRldmVsb3BlciBiYXNlZCBpbiBGaW5sYW5kIVxuICAgICAgPC9Cb3g+XG4gICAgICAgIDxkaXY+SGVsbG8hPC9kaXY+XG4gICAgPC9Db250YWluZXI+XG4gIClcbn1cblxuZXhwb3J0IGRlZmF1bHQgUGFnZVxuIl0sIm5hbWVzIjpbIkNvbnRhaW5lciIsIkJveCIsIlBhZ2UiLCJib3JkZXJSYWRpdXMiLCJkaXYiXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./pages/index.js\n");
+
+/***/ }),
+
+/***/ "@chakra-ui/react":
+/*!***********************************!*\
+  !*** external "@chakra-ui/react" ***!
+  \***********************************/
+/***/ ((module) => {
+
+module.exports = require("@chakra-ui/react");
+
+/***/ }),
+
+/***/ "react/jsx-dev-runtime":
+/*!****************************************!*\
+  !*** external "react/jsx-dev-runtime" ***!
+  \****************************************/
+/***/ ((module) => {
+
+module.exports = require("react/jsx-dev-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("./pages/index.js"));
+module.exports = __webpack_exports__;
+
+})();
